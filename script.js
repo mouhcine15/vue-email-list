170d6147e4c36a4245e8a52cf0120a2e361a567a
mouhcine <mouhcine15.00@gmail.com>
2022-06-06 16:26:24 +0200

    prova ciclo

--- script.js
+++ script.js
@@ -1,87 +1,94 @@
 const app = new Vue ({
     el: "#app",
     data: {
-        emailUser1: "",
-        emailUser2: "",
-        emailUser3: "",
-        emailUser4: "",
-        emailUser5: "",
-        emailUser6: "",
-        emailUser7: "",
-        emailUser8: "",
-        emailUser9: "",
-        emailUser10: ""
+        email: [
+            {
+                emailUser1: ""
+            }
+        ]
+        // emailUser2: "",
+        // emailUser3: "",
+        // emailUser4: "",
+        // emailUser5: "",
+        // emailUser6: "",
+        // emailUser7: "",
+        // emailUser8: "",
+        // emailUser9: "",
+        // emailUser10: ""
     },
     mounted() {
-        
+        for (let i = 0; i < 10; i++) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((result) => {
                 const risultato = result.data.response;
                 console.log(risultato);
                 this.emailUser1=risultato;
             });
+            
+            
+        }
 
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((result) => {
-                const risultato = result.data.response;
-                console.log(risultato);
-                this.emailUser2=risultato;
-            });
+            // axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            // .then((result) => {
+            //     const risultato = result.data.response;
+            //     console.log(risultato);
+            //     this.emailUser2=risultato;
+            // });
 
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((result) => {
-                const risultato = result.data.response;
-                console.log(risultato);
-                this.emailUser3=risultato;
-            });
+            // axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            // .then((result) => {
+            //     const risultato = result.data.response;
+            //     console.log(risultato);
+            //     this.emailUser3=risultato;
+            // });
 
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((result) => {
-                const risultato = result.data.response;
-                console.log(risultato);
-                this.emailUser4=risultato;
-            });
+            // axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            // .then((result) => {
+            //     const risultato = result.data.response;
+            //     console.log(risultato);
+            //     this.emailUser4=risultato;
+            // });
 
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((result) => {
-                const risultato = result.data.response;
-                console.log(risultato);
-                this.emailUser5=risultato;
-            });
+            // axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            // .then((result) => {
+            //     const risultato = result.data.response;
+            //     console.log(risultato);
+            //     this.emailUser5=risultato;
+            // });
 
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((result) => {
-                const risultato = result.data.response;
-                console.log(risultato);
-                this.emailUser6=risultato;
-            });
+            // axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            // .then((result) => {
+            //     const risultato = result.data.response;
+            //     console.log(risultato);
+            //     this.emailUser6=risultato;
+            // });
 
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((result) => {
-                const risultato = result.data.response;
-                console.log(risultato);
-                this.emailUser7=risultato;
-            });
+            // axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            // .then((result) => {
+            //     const risultato = result.data.response;
+            //     console.log(risultato);
+            //     this.emailUser7=risultato;
+            // });
 
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((result) => {
-                const risultato = result.data.response;
-                console.log(risultato);
-                this.emailUser8=risultato;
-            });
+            // axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            // .then((result) => {
+            //     const risultato = result.data.response;
+            //     console.log(risultato);
+            //     this.emailUser8=risultato;
+            // });
 
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((result) => {
-                const risultato = result.data.response;
-                console.log(risultato);
-                this.emailUser9=risultato;
-            });
+            // axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            // .then((result) => {
+            //     const risultato = result.data.response;
+            //     console.log(risultato);
+            //     this.emailUser9=risultato;
+            // });
 
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((result) => {
-                const risultato = result.data.response;
-                console.log(risultato);
-                this.emailUser10=risultato;   
-            });
+            // axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            // .then((result) => {
+            //     const risultato = result.data.response;
+            //     console.log(risultato);
+            //     this.emailUser10=risultato;   
+            // });
     }
 });
